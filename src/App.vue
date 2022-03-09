c3257339bbd1f35becddc18f08ca95745542d9a2
Vinicius Marques <vini_marques@uni9.edu.br>
2022-03-08 22:22:06 -0300

    primeiro commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="a"> 
+    <navBar/>
+    <div class="container w-25 p-3 bg-light mx-auto opacity-75" style="margin-top: 120px;">
+      <div>
+        <login/>
+      </div>
+    </div>
+  </div>
+  
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import navBar from './components/navBar.vue'
+import login from './components/login.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    login,
+    navBar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,body{
+background: url('http://www.osmais.com/wallpapers/201410/grande-lago-azul-wallpaper.jpg');
+background-repeat: no-repeat;
+background-size: cover;
+
+
 }
+
 </style>
---
+++ src/components/navBar.vue
@@ -0,0 +1,21 @@
+<template>
+   <nav class="navbar navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="../assets/logo.png" alt="" width="30" height="30" class="d-inline-block align-text-top">
+      Projeto_telaLogin
+    </a>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
